7c4d25e206c33070c2c1fb58ce1deb49928aa8cc
Heng Xin <heng.xin@qq.com>
2016-10-30 17:11:33 +0800

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
-
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest, time
+import time
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
         self.assertIn("To-Do", self.browser.title)
         header_text = self.browser.find_element_by_tag_name("h1").text
         self.assertIn("To-Do", header_text)
@@ -28,17 +28,15 @@ class NewVisitorTest(unittest.TestCase):
             inputbox.get_attribute("placeholder"),
             "Enter a to-do item"
         )
-        inputbox.send_keys("1: Buy peacock feathers")
+        inputbox.send_keys("Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
         self.check_for_row_in_list_table("1: Buy peacock feathers")
         time.sleep(2)
         inputbox = self.browser.find_element_by_id("id_new_item")
-        inputbox.send_keys("2: Use peacock feathers to make a fly")
+        inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(2)
         self.check_for_row_in_list_table("1: Buy peacock feathers")
         self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
         self.fail("Finish Test")
 
-if __name__ == '__main__':
-    unittest.main(warnings="ignore")
